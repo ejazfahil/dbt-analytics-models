0e5c63b8620c1d82d563ea6416033ba561977d14
Fahil Ejaz <ejazfahil@gmail.com>
2023-03-23 11:45:00 +0100

    feat: add staging models for raw sources

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,12 @@
+-- stg_orders 2023-03-23
+{{ config(materialized='view') }}
+
+SELECT
+    order_id::TEXT                    AS order_id,
+    customer_id::TEXT                 AS customer_id,
+    order_date::DATE                  AS order_date,
+    status::TEXT                      AS status,
+    total_amount::NUMERIC             AS total_amount_usd,
+    _loaded_at                        AS loaded_at
+FROM {{ source('raw', 'orders') }}
+WHERE order_id IS NOT NULL
